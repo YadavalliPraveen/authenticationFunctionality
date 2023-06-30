4869e1ce6244812a1c3c9b1692776528c5120e35
Yadavalli Praveen <yadavallipraveen2424@gmail.com>
2023-06-30 08:27:49 +0000

    authentication Functionality

---
+++ src/components/Login/index.js
@@ -0,0 +1,47 @@
+import {Component} from 'react'
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import './index.css'
+
+class Login extends Component {
+  onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    history.replace('/')
+  }
+
+  submitForm = async event => {
+    event.preventDefault()
+    const username = 'rahul'
+    const password = 'rahul@2021'
+    const userDetails = {username, password}
+
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(data)
+
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    }
+  }
+
+  render() {
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+
+    return (
+      <form className="login-container" onSubmit={this.submitForm}>
+        <h1>Please Login</h1>
+        <button type="submit">Login with sample creds</button>
+      </form>
+    )
+  }
+}
+export default Login
